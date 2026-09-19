bf60a0f7772dd0ee7f1152ab781efc4e4d06d836
AhmadZaid20187 <ahmadzaid20187@gmail.com>
2026-09-19 13:21:33 +0600

    Heading added

--- src/app/page.js
+++ src/app/page.js
@@ -30,6 +30,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 
+      <h2 className="text-5xl font-bold">Actors Name:</h2>
       {
         actors.map(actor => <Actor key={actor.id} actor={actor} ></Actor>)
       }
